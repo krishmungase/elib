d07c6a3baf0f31c4c8e65734f66fd4d04680f15b
krishmungase <mungasekrishna8@gmail.com>
2024-08-03 16:30:07 +0530

    user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,12 +1,29 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.json({message: "User created"})
+
+
+  // Validation
+  const { name, email, password } = req.body;
+
+  if(!name || !email || !password){
+    const error = createHttpError(400, "All required fields");
+    return next(error);
+  }
+
+  // process
+
+
+  // Response
+
+
+  res.json({ message: "User created" })
 };
 
 
-export {createUser};
+export { createUser };
