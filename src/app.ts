d71ef43692fbf0fd16aeab542d47e7ae03467b21
krishmungase <mungasekrishna8@gmail.com>
2024-08-03 16:09:15 +0530

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 const app = express();
 
 
 // Routes
-app.get('/', (req, res,next) => {
-  res.json({message: "Welcome!"});
+app.get('/', (req, res, next) => {
+  res.json({ message: "Welcome!" });
 })
 
+// global error handler
+app.use(globalErrorHandler)
 
 export default app;
