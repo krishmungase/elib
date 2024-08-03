c78cb06ccf27f8aa93dda1d45a9d42983fb5a334
krishmungase <mungasekrishna8@gmail.com>
2024-08-03 19:32:38 +0530

    login route & validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -51,6 +51,44 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
 
 const loginUser = async (req:Request, res:Response,next:NextFunction) => {
+
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    const error = createHttpError(400, "All required fields");
+    return next(error);
+  }
+
+  let user;
+  try {
+    user = await userModel.findOne({email});
+    if(!user){
+      return next(createHttpError(400, "User not found"));
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error fetching user"));
+  }
+
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if(!isMatch){
+    return next(createHttpError(401, "Incorrect password"));
+  }
+
+
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+    res.status(201).json({ 
+      accessToken: token ,
+      message: "User signed in successfully"
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while Logging"));
+  }
+
   res.json({message: "All Ok!"});
 }
 
