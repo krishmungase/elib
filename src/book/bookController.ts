4e43ec123adbe5ae6c2ec07a4efe8d2866c113f3
krishmungase <mungasekrishna8@gmail.com>
2024-08-04 09:27:30 +0530

    file image and upload on cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,27 +1,50 @@
 import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
-import path from "node:path";
+import path, { format } from "node:path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) =>{
 
   // const {} = req.body;
-  console.log('files', req.files);
-  const files = req.files as {[fieldname: string]: Express.Multer.File[]}
-  const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
-  const fileName = files.coverImage[0].filename;
-  const filePath = path.resolve(__dirname, '../../public/data/uploads',fileName);
 
-  const uploadResult = await cloudinary.uploader.upload(filePath,{
-    filename_override : fileName,
-    folder : 'book-covers',
-    format : coverImageMimeType,
-  })
 
-  console.log("Upload result: " + uploadResult);
 
+
+  try {
+
+    console.log('files', req.files);
+    const files = req.files as {[fieldname: string]: Express.Multer.File[]}
+    const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(__dirname, '../../public/data/uploads',fileName);
   
+    const uploadResult = await cloudinary.uploader.upload(filePath,{
+      filename_override : fileName,
+      folder : 'book-covers',
+      format : coverImageMimeType,
+    })
+  
+    console.log("Upload result: " + uploadResult);
 
-  res.json({})
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookFileName);
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath,{
+      filename_override : bookFileName,
+      folder: 'book-pdfs',
+      format: "pdf"
+    })
+    console.log("bookfileUploadResult: " + bookFileUploadResult)
+    res.json({})
+
+
+  } catch (error) {
+    console.log(error)
+    return next(createHttpError(500,"Error whilte uploading book"))
+  }
+
+
 }
 
 export {createBook};
